3e30da58aa34ee9793ea9aef5e9fe56fa3a4797e
tran pham <trannpham.05@gmail.com>
2023-08-28 09:56:51 -0500

    added private methods

--- src/Fraction.java
+++ src/Fraction.java
@@ -14,7 +14,21 @@ public class Fraction {
         denominator = d;
     }
 
-    
+    private static int gcd(int a, int b)
+    {
+        if(b == 0)
+        {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private static int lcm(int a, int b)
+    {
+        return (a / gcd(a, b)) * b;
+    }
+
+
 }
 
 /*
